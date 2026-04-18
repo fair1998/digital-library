641403ce15fa843c5a5a5f4825571883f984d95d
Fair <fair1998@hotmail.com>
2026-04-18 18:35:26 +0700

    Refactor settings: set DEBUG to True and remove RENDER environment variable check; delete runtime.txt

--- config/settings.py
+++ config/settings.py
@@ -10,13 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-RENDER = os.getenv("RENDER") == "true"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -25,7 +22,7 @@ RENDER = os.getenv("RENDER") == "true"
 SECRET_KEY = 'django-insecure-7sd95*88vp3pvo*r+x2)r$_+b#f5h4dfs757p-_q_v3jy+i%+y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = not RENDER
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
